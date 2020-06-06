2fb39b7480a7210a3043c3431037ae5653c9476d
Bach Hong Thai <bachhongthai@Hong-Thai-Bach.local>
2020-06-06 15:31:57 +0700

    upload

--- src/compoment/view/Home.js
+++ src/compoment/view/Home.js
@@ -1,8 +1,9 @@
-import React,{useState, useEffect} from 'react'
+import React,{useState, useEffect,useContext} from 'react'
+import {UserContext} from '../../App'
 
 const Home = () => {
-
     const [data, setData] = useState([])
+    const {state, dispatch} = useContext(UserContext)
     useEffect(()=>{
         fetch('/newsfeed',{
             headers:{
@@ -10,10 +11,89 @@ const Home = () => {
             }
         }).then(res=>res.json())
         .then(result => {
+            console.log(result)
             setData(result.posts)
         })
     },[])
 
+    const likePost = (id) => {
+        fetch('/like', {
+            method: "put",
+            headers:{
+                "Content-Type":"application/json", 
+                "Authorization": "" + localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res => res.json())
+        .then(result => {
+            //console.log(result)
+            const newData = data.map(item => {
+                if(item._id == result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    const unlikePost = (id) => {
+        fetch('/unlike', {
+            method: "put",
+            headers:{
+                "Content-Type":"application/json", 
+                "Authorization": "" + localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res => res.json())
+        .then(result => {
+           // console.log(result)
+            const newData = data.map(item => {
+                if(item._id == result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    const makeComment = (text, postId) =>{
+        fetch('/comment', {
+            method:"put",
+            headers:{
+                "Content-Type":"application/json", 
+                "Authorization": "" + localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res => res.json())
+        .then(result => {
+            console.log(result)
+            const newData = data.map(item => {
+                if(item._id == result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
     return(
         <div className="home">
             {
@@ -26,9 +106,36 @@ const Home = () => {
                         </div>
                         <div className="card-content">
                             <i className="material-icons" style={{color:"red"}}>favorite</i>
+                            {item.likes.includes(state._id)
+                               ? 
+                               <i className="material-icons"
+                                   onClick={()=>{unlikePost(item._id)}}>thumb_down</i>
+                               : 
+                               <i className="material-icons"
+                                   onClick={()=>{likePost(item._id)}}>thumb_up</i>
+                            }
+                            <h6>{item.likes.length} likes</h6>
                             <h6>{item.title}</h6>
                             <p>{item.body}</p>
-                            <input type="text" placeholder="comment"/>
+                            {
+                                item.comments.map(record => {
+                                    return(
+                                    <h6 key={record._id}>
+                                        <span style={{fontWeight:"500"}}>
+                                        {record.postedBy.name}
+                                        </span> 
+                                        {record.text}
+                                    </h6>
+                                    )
+                                })
+                            }
+                            <form onSubmit={(e) => {
+                                e.preventDefault()
+                                makeComment(e.target[0].value, item._id)
+                                //makeComment(e.target[0].value, item._id)
+                            }}>
+                                <input type="text" placeholder="comment"/>
+                            </form>
                         </div>
                         </div>
                     )
